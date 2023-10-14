6ded41fab72abc99596cdb3b7b7ca1fd9fe868a6
Nelia <nelia.courtais@live.fr>
2023-10-14 17:19:59 +0200

    change style

--- wildCard/src/App.jsx
+++ wildCard/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./style/style.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <firstPart>
+        <span>
+          <img
+            src="src/assets/logoWildCodeSchool.webp"
+            alt="logoWildCodeSchool"
+          />
+        </span>
+        <text>
+          <h1>Wild Code School</h1>
+          <h2>Learn code faster</h2>
+        </text>
+      </firstPart>
+
+      <secondPart>
+        <h2>Do you like it?</h2>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/3/35/Emoji_u2665.svg"
+          alt="loveSchool"
+        />
+      </secondPart>
+    </main>
+  );
 }
 
-export default App
+export default App;
